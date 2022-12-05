f6399e709816f69a3cd8ddf7ed5bd6f44b1a4d49
Galla Dindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2022-12-05 22:42:56 +0100

    Update fetchCountries.js

--- src/JS/fetchCountries.js
+++ src/JS/fetchCountries.js
@@ -10,17 +10,3 @@ export function fetchCountries(name) {
   });
 }
 
-// name.official - полное имя страны
-// capital - столица
-// population - население
-// flags.svg - ссылка на изображение флага
-// languages - массив языков
-
-// https://restcountries.com/v3.1/name/{name}?fullText=true
-// https://restcountries.com/v3.1/capital/{capital}
-
-// "flags": {
-//   "svg": "https://restcountries.com/data/per.svg",
-//   "png": "https://restcountries.com/data/png/per.png"
-// }
-// https://restcountries.com/v3.1/lang/{lang}
